263cd2583f7cc08f04ae1b6429e4bbd472436bac
sandina <alejandro.sandin@roche.com>
2022-01-26 09:19:02 +0100

    Initial commit

---
+++ src/main/java/com/sandin/learning/springbootwebdemo/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.sandin.learning.springbootwebdemo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    @ResponseBody
+    public String loginMessage(){
+
+        return "Hello world!";
+    }
+}
